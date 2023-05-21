e6e5172610bc923c9a8041f9a042ab9bbe9ed972
brian <90752841+wokbjso@users.noreply.github.com>
2023-05-21 16:22:41 +0900

    wge
    weg

--- 김현민/CompanyMember.cpp
+++
@@ -1,7 +0,0 @@
-#include "CompanyMember.h"
-
-void CompanyMember::CompanyMember(string name, int number, string id, string password) : Member(id, password)
-{
-    this.companyName = name;
-    this.companyNumber = number;
-}
